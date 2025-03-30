d282c6082cf6d4d2adb3d1640e2fd0a53f110564
Rishabh Anand <rishabhanand42@gmail.com>
2025-03-31 03:26:11 +0530

    update build with latest info

--- docs/assets/Me-COzCuDuL.js
+++ docs/assets/Me-COzCuDuL.js
@@ -1,25 +1,25 @@
-import{j as e}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as i}from"./index-CcnH5Kt0.js";import"./index-RYns6xqu.js";function o(r){const n={code:"code",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",ul:"ul",...i(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{id:"hey-rishabh-here-",children:"Hey! Rishabh here 👋"}),`
+import{j as e}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as o}from"./index-CcnH5Kt0.js";import"./index-RYns6xqu.js";function i(r){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",ul:"ul",...o(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{id:"hey-rishabh-here-",children:"Hey! Rishabh here 👋"}),`
 `,e.jsx(n.p,{children:"Software developer from Bangalore. Graduated with Computer Science '21 from BITS Pilani, Goa. Believe in the power of education and a big fan of opensource development."}),`
 `,e.jsx("a",{href:"https://www.linkedin.com/in/rish-0-0",rel:"nofollow noreferrer",children:e.jsx("img",{src:"https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"})}),`
 `,e.jsx("a",{href:"https://leetcode.com/u/rish-0-0/",rel:"nofollow noreferrer",children:e.jsx("img",{src:"https://img.shields.io/badge/-LeetCode-FFA116?style=for-the-badge&logo=LeetCode&logoColor=black"})}),`
-`,e.jsx("a",{href:"https://github.com/rish-0-0",rel:"nofollow noreferrer",children:e.jsx("img",{src:"https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white"})}),`
 `,e.jsx(n.h2,{id:"my-npm-packages",children:"My NPM Packages"}),`
-`,e.jsx(n.h3,{id:"neo-flow-viz",children:"Neo-Flow-Viz"}),`
+`,e.jsx(n.h3,{id:"new-flow-viz",children:e.jsx(n.a,{href:"https://rishabh-anand.com/neo-flow-viz",rel:"nofollow",children:"New-Flow-Viz"})}),`
 `,e.jsx(n.p,{children:"It is a Neo4j Graph visualization Library for React Projects, written in Typescript. It uses ReactFlow as the underlying Graph Library and the response and Query Format it builds on is for Neo4j."}),`
 `,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`npm install --save neo-flow-viz
 `})}),`
-`,e.jsx(n.h3,{id:"astroreha",children:"Astroreha"}),`
-`,e.jsx(n.p,{children:"Astrological Calculations and Chart Generating Library. Got 10 ⭐s"}),`
+`,e.jsx(n.h3,{id:"planets",children:e.jsx(n.a,{href:"https://rishabh-anand.com/rish-0-0",rel:"nofollow",children:"Planets"})}),`
+`,e.jsx(n.p,{children:"ThreeJS simulation of 3-body problem"}),`
 `,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`npm install --save astroreha
 `})}),`
 `,e.jsxs(n.ul,{children:[`
-`,e.jsxs(n.li,{children:["🔭 I'm currently working on ",e.jsx(n.code,{children:"mae"}),". It is a tool for management and employees to get at better together at management 🤝."]}),`
-`,e.jsx(n.li,{children:"⚙️ I have 3.5y of Software Development Experience working in a ☁️ Cloud Security / Data Security Company (DSPM if anyone is interested)."}),`
+`,e.jsx(n.li,{children:"🔭 I'm currently learning ThreeJS. Some 3d animation has struck my interest."}),`
+`,e.jsx(n.li,{children:"⚙️ I have 4y of Software Development Experience working in a ☁️ Cloud / Data / Privacy Security."}),`
 `,e.jsx(n.li,{children:"🔥 My major strength would be the ability to learn very quickly."}),`
 `,e.jsxs(n.li,{children:["🌱 I’m currently learning ",e.jsx(n.code,{children:"elixir"}),", ",e.jsx(n.code,{children:"golang"}),", ",e.jsx(n.code,{children:"rust"})," and ",e.jsx(n.code,{children:"webassembly"}),"."]}),`
-`,e.jsx(n.li,{children:"👯 I'm a big fastify fan. Shotout @mcollina. I have also written a fastify CRUD (REST APIs) generator for Postgres via Sequelize. We call it FSC at our company."}),`
+`,e.jsx(n.li,{children:"👯 I'm a big fastify fan. Shotout @mcollina. I have written a fastify CRUD generator. Currently privated."}),`
 `,e.jsxs(n.li,{children:["💬 Ask me about anything ",e.jsx(n.code,{children:"react"}),". It's my favourite choice for developing websites."]}),`
 `,e.jsxs(n.li,{children:["📫 How to reach me: ",e.jsx(n.code,{children:"rishabhanand42@gmail.com"})]}),`
 `,e.jsxs(n.li,{children:["⚡ Fun fact: ",e.jsx(n.code,{children:"GraphQL"})," has nothing to do with Graph Databases 😂"]}),`
-`,e.jsx(n.li,{children:"😠 I hate redux, and would do anything to get around it 😂"}),`
-`]})]})}function l(r={}){const{wrapper:n}={...i(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(o,{...r})}):o(r)}export{l as default};
+`,e.jsxs(n.li,{children:["⚡ Fun fact: I have gotten 10 ⭐s and 10 forks on a ",e.jsx(n.a,{href:"https://github.com/rish-0-0/astroreha",rel:"nofollow",children:"repo"}),"."]}),`
+`,e.jsx(n.li,{children:"😠 I like redux, but would do anything to get around it 😂"}),`
+`]})]})}function t(r={}){const{wrapper:n}={...o(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(i,{...r})}):i(r)}export{t as default};
